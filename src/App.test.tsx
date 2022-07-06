6809c33118b86c0ccd5279be685e3da99fce6e1b
kei95 <kei0613shine.star@gmail.com>
2022-07-05 22:37:12 -0700

    My attempt in an hour

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App, { User } from "./App";
 
-test('renders learn react link', () => {
+const MOCK_LIST: User[] = [
+  {
+    login: "Lorem Ipsum",
+    id: 14291380,
+  },
+  {
+    login: "de Finibus Bonorum et Malorum",
+    id: 42897193,
+  },
+];
+
+global.fetch = Promise.resolve({
+  json: () => Promise.resolve(MOCK_LIST),
+}) as any;
+
+// TODO: figure out how to mock global fetch
+test("renders my account name with load button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const myName = screen.getByText(/kei95/i);
+  const buttonText = screen.getByText("load");
+
+  expect(myName).toBeInTheDocument();
+  expect(buttonText).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+// ========= constant =========
+const MY_ACCOUNT_NAME = "kei95";
+
+// ========== Definition ==========
+export interface User {
+  login: string;
+  id: number;
+}
+
+const ROW_STATE = {
+  LOADING: "LOADING",
+  OPEN: "OPEN",
+  DEFAULT: "DEFAULT",
+} as const;
+
+type RowState = keyof typeof ROW_STATE;
+
+// ========== Util ==========
+async function fetchFollowers(username: string): Promise<User[]> {
+  const res = await fetch(`https://api.github.com/users/${username}/following`);
+
+  if (res.ok) {
+    const jsonizedRes = (await res.json()) as User[];
+    return jsonizedRes;
+  }
+  return [];
+}
+
+// ========== Component ==========
+function UserRow({ user }: { user: User }) {
+  const [userList, setUserList] = useState<User[]>([]);
+  const [state, setState] = useState<RowState>(ROW_STATE.DEFAULT);
+  const buttonText = state === ROW_STATE.LOADING ? "loading..." : "load";
+
+  const handlePressButton = async () => {
+    setState(ROW_STATE.LOADING);
+    const res = await fetchFollowers(user.login);
+    setState(ROW_STATE.OPEN);
+    setUserList(res);
+  };
+
+  return (
+    <ul>
+      <li key={user.id}>
+        {user.login}
+        {state !== "OPEN" ? (
+          <button
+            onClick={handlePressButton}
+            className={state === "LOADING" ? "loadingButton" : ""}
+          >
+            {buttonText}
+          </button>
+        ) : null}
+        {/* list of the user's followers */}
+        {userList.map((user) => (
+          <UserRow user={user} />
+        ))}
+      </li>
+    </ul>
+  );
+}
 
 function App() {
+  const [userList, setUserList] = useState<User[]>([]);
+
+  // Fetch my followers as initial list
+  useEffect(() => {
+    async function fetchMyFollowers() {
+      const followers = await fetchFollowers(MY_ACCOUNT_NAME);
+      setUserList(followers);
+    }
+    fetchMyFollowers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li>{MY_ACCOUNT_NAME}</li>
+      {userList.map((user) => (
+        <UserRow user={user} />
+      ))}
+    </ul>
   );
 }
 
